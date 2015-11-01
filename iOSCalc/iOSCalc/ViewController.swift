5d13a5dfe6d1650f527f9f6e3451a0caba21ccf3
Kevin Ly <kevinly7@uw.edu>
2015-10-31 22:38:23 -0700

    saving work

--- iOSCalc/iOSCalc/ViewController.swift
+++ iOSCalc/iOSCalc/ViewController.swift
@@ -21,16 +21,19 @@ class ViewController: UIViewController {
     }
     
     var isTyping = false
-    var firstNumber = 0
-    var secondNumber = 0
+    var total : Double? = nil
+    var current : Double? = nil
     var operation = ""
-    var displayCount = 0
+    var arrayCount = [Double]()
+    var arrayAvg = [Double]()
+    var arrayTotal : Double = 0.0
+    var canCount = false
     
     @IBOutlet weak var display: UILabel!
     
     @IBAction func numPressed(sender: AnyObject) {
+        canCount = true;
         let number = sender.currentTitle
-        
         if isTyping {
             display.text = display.text! + number!!
         } else {
@@ -38,5 +41,102 @@ class ViewController: UIViewController {
             isTyping = true
         }
     }
+    
+    @IBAction func calculate(sender: UIButton) {
+        isTyping = false
+        current = Double(display.text!)!
+        if operation != "" {
+            switch operation {
+                case "+":
+                    total = total! + current!
+                case "-":
+                    total = total! - current!
+                case "*":
+                    total = total! * current!
+                case "/":
+                    total = total! / current!
+                case "MOD":
+                    total = total! % current!
+                default:
+                    break
+                }
+        } else {
+            total = current
+        }
+        display.text = "\(total!)"
+        operation = sender.currentTitle!
+    }
+    
+    @IBAction func count(sender: UIButton) {
+        isTyping = false
+        if canCount {
+            current = Double(display.text!)
+            arrayCount.append(current!)
+            total = Double(arrayCount.count)
+            display.text = "\(total!)"
+            canCount = false
+        }
+    }
+    
+    @IBAction func average(sender: UIButton) {
+        isTyping = false
+        if canCount {
+            current = Double(display.text!)
+            arrayTotal += Double(current!)
+            arrayAvg.append(current!)
+            total = arrayTotal / Double(arrayAvg.count)
+            display.text = "\(total!)"
+            canCount = false
+        }
+    }
+    
+    @IBAction func fact(sender: UIButton) {
+        
+    }
+
+    @IBAction func equals(sender: UIButton) {
+        isTyping = false
+        current = Double(display.text!)!
+        
+        switch operation {
+            case "+":
+                total = total! + current!
+            case "-":
+                total = total! - current!
+            case "X":
+                total = total! * current!
+            case "/":
+                total = total! / current!
+            case "MOD":
+                total = total! % current!
+            default:
+                break
+        }
+        if total != nil {
+            display.text = "\(total!)"
+        }
+        isTyping = false
+        total = nil
+        current = nil
+        operation = ""
+        arrayCount = [Double]()
+        arrayAvg = [Double]()
+        canCount = false
+    }
+    
+    @IBAction func clear(sender: UIButton) {
+        isTyping = false
+        total = nil
+        current = nil
+        operation = ""
+        arrayCount = [Double]()
+        display.text = "0"
+        canCount = false
+        arrayTotal = 0.0
+        arrayAvg = [Double]()
+    }
+    
+    
+    
 }
 
